1ee5600b026f10c43fd7cb80d6efeec7a4278779
Ablaymillen <80356093+Ablaymillen@users.noreply.github.com>
2021-10-08 23:54:22 +0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
-import pandas as pd
-from pycoingecko import CoinGeckoAPI
+import sys
+sys.path.append('src')
+import src
 
 num_rows = int(input("Enter the number of rows which will be printed: "))
 #Making request from api and printing the rows of top most crypto currencies
@@ -9,8 +10,8 @@ read_API(num_rows)
 #Save the data frame retrieved from read_API() function into variable
 #Use store_data function and pass this variable
 #---------------stores data in CSV format------------------#
-data_frame = read_API(num_rows)
-store_data(data_frame)  
+data_frame = src.read_API(num_rows)
+src.store_data(data_frame)  
 
 
 
